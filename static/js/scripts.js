d551ae5016607b128e562d4156c44be412a3c474
LuisHCK <luisjcenteno@live.com>
2017-02-16 14:13:00 -0600

    Añadido toggle para formulario de servicios

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
     $("#vender").click(function () {
-        $("#form_vender").toggle(1000);
+        $("#form_vender").toggle(500);
+    });
+    $("#servicio").click(function () {
+        $("#form_servicio").toggle(500);
     });
 });
