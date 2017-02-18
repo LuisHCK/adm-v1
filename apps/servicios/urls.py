042fda86d25dd6fdb07b3679e13dbe9a7eb372fc
LuisHCK <luisjcenteno@live.com>
2017-02-17 18:08:34 -0600

    Facturar servicios con un botón

--- apps/servicios/urls.py
+++ apps/servicios/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^nuevo/$', views.realizar_servicio, name='realizar_servicio'),
     url(r'^$', views.inicio, name='servicios_realizados'),
+    url(r'^agregar_a_factura/(?P<pk>[0-9]+)/(?P<fact>[0-9]+)$', views.agregar_a_factura, name='agregar_a_factura'),
 ]
--- apps/servicios/views.py
+++ apps/servicios/views.py
@@ -1,8 +1,9 @@
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from apps.caja.models import Caja
+from apps.facturas.models import Factura, FacturaItems
 from .models import Servicio, TipoServicio
 from .forms import ServicioForm
 
@@ -12,7 +13,9 @@ from .forms import ServicioForm
 def inicio(request):
     """Devuelve los servicios realizados"""
     servicios = Servicio.objects.all().order_by('-fecha_servicio')
-    return render(request, 'servicios/servicios.html', {'servicios': servicios})
+    facturas = Factura.objects.filter(cobrada=False)
+    return render(request, 'servicios/servicios.html',
+                  {'servicios': servicios, 'facturas': facturas})
 
 
 def realizar_servicio(request):
@@ -25,13 +28,13 @@ def realizar_servicio(request):
             servicio.usuario = request.user
 
             # Asignar el precio del servicio segun el tipo de servicio
-            tiposervicio = TipoServicio.objects.get(id=servicio.tipo_servicio.id)
+            tiposervicio = TipoServicio.objects.get(
+                id=servicio.tipo_servicio.id)
             servicio.tipo_servicio = tiposervicio
 
             # Calcular el precio en base a la cantidad de producto
-            servicio.precio = (tiposervicio.costo*servicio.cantidad)
+            servicio.precio = (tiposervicio.costo * servicio.cantidad)
             servicio.descripcion = tiposervicio.nombre
-
 
             servicio.save()
 
@@ -44,3 +47,17 @@ def realizar_servicio(request):
     else:
         form = ServicioForm()
     return render(request, 'servicios/servicio_form.html', {'form': form})
+
+
+def agregar_a_factura(request, pk, fact):
+    """Agrega un servicio como item de factura"""
+    servicio = get_object_or_404(Servicio, id=pk)
+    factura = get_object_or_404(Factura, id=fact)
+    FacturaItems.objects.create(
+        factura=factura,
+        concepto=servicio.descripcion,
+        precio=servicio.precio
+        )
+    factura.total += servicio.precio
+    factura.save()
+    return redirect('servicios_realizados')
