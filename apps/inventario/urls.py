29e30c0c76be84da739094e6207fdc71190a0e9e
LuisHCK <luisjcenteno@live.com>
2017-03-06 23:25:37 -0600

    Crear y eliminar artículos usando ajax

--- apps/inventario/urls.py
+++ apps/inventario/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     #url(r'^$', views.inicio, name='inicio'),
     url(r'^$', views.lista_inventario, name='ver_inventario'),
     url(r'^articulo/(?P<pk>[0-9]+)/$', views.detalles_articulo, name='ver_articulo'),
-    url(r'^existencias/(?P<pk>[0-9]+)/editar/$', views.actualizar_existencias, name='act_existencias'),
+    url(r'^existencias/(?P<pk>[0-9]+)/editar/$', views.actualizar_existencias,
+        name='act_existencias'),
     url(r'^articulo/nuevo', views.nuevo_articulo, name='crear_articulo'),
+    url(r'^articulo/ajax', views.nuevo_articulo_ajax, name='ajax_crear_articulo'),
+    url(r'^eliminar/(?P<pk>[0-9]+)/$', views.eliminar_articulo_ajax, name='eliminar_articulo')
 ]
--- apps/inventario/views.py
+++ apps/inventario/views.py
@@ -1,6 +1,7 @@
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
+import json
 from apps.inventario.models import Inventario, Articulos
 from .forms import ArticuloForm, InventarioForm
 
@@ -10,17 +11,16 @@ from .forms import ArticuloForm, InventarioForm
 @login_required(login_url='login')  # redirect when user is not logged in
 def lista_inventario(request):
     """"Retorna la lista de los articulos en el inventario"""
-    inventario = Inventario.objects.select_related().all()
+    inventario = Inventario.objects.select_related().all().order_by('-id')
     # calcular el total de dinero invertido en el Inventario
     total_inversion = 0
     for inv in inventario:
         total_inversion += (inv.articulo.precio_venta * inv.existencias)
-
-
     return render(request, 'inventario/lista_inventario.html', {
         'inventario': inventario,
-        'total_inversion': total_inversion
-        })
+        'total_inversion': total_inversion,
+        'form_articulo': ArticuloForm,
+    })
 
 
 def detalles_articulo(request, pk):
@@ -46,6 +46,34 @@ def nuevo_articulo(request):
     return render(request, 'inventario/editar_articulo.html', {'form': form})
 
 
+def nuevo_articulo_ajax(request):
+    """Crear un nuevo articulo"""
+    response_data = {}
+    if request.method == "POST":
+        form = ArticuloForm(request.POST)
+        if form.is_valid():
+            articulo = form.save(commit=False)
+            articulo.save()
+        # Agregar el producto al inventario
+        Inventario.objects.create(articulo=articulo, existencias=0)
+
+        response_data['result'] = "Se creó correctamente el artículo"
+        response_data['articulo_id'] = str(articulo.id)
+        response_data['nombre'] = str(articulo)
+        response_data['precio_venta'] = str(articulo.precio_venta)
+        response_data['precio_compra'] = str(articulo.precio_compra)
+
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json"
+        )
+    else:
+        return HttpResponse(
+            json.dumps({"nothing to see": "this isn't happening"}),
+            content_type="application/json"
+        )
+
+
 def actualizar_existencias(request, pk):
     """Edita las existencias de un articulo en el inventario"""
     inventario = get_object_or_404(Inventario, pk=pk)
@@ -58,3 +86,26 @@ def actualizar_existencias(request, pk):
     else:
         form = InventarioForm(instance=inventario)
     return render(request, 'inventario/editar_existencias.html', {'form': form})
+
+
+def eliminar_articulo_ajax(request, pk):
+    """Elimina un articulo"""
+    response_data = {}
+
+    if request.method == "POST":
+        articulo = Articulos.objects.get(pk=pk)
+        response_data['articulo_id'] = str(articulo.id)
+        articulo.delete()
+        response_data['result'] = "Se eliminó con exito"
+        response_data['articulo'] = str(articulo)
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json"
+        )
+    else:
+        response_data['result'] = "Ocurrió un error al realizar la acción"
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json",
+            status=410,
+        )
