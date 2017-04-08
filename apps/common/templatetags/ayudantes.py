896780fc1099c2d47415fcff317e3043fe879b46
luishck <luisjcenteno@live.com>
2017-04-08 00:15:12 -0600

    Ayudante para obtener la foto de un usuario

--- apps/common/templatetags/ayudantes.py
+++ apps/common/templatetags/ayudantes.py
@@ -1,5 +1,10 @@
 from django import template
 from django.contrib.auth.models import Group 
+from django.shortcuts import get_object_or_404
 
 register = template.Library() 
 
+@register.filter(name='avatar')
+def avatar(user):
+    avatar_perfil = user.perfil.foto.url
+    return avatar_perfil
