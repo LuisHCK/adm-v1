352f5304ad1f0bd28fbbb2a98aa25a5a8b15d126
luishck <luisjcenteno@live.com>
2017-04-25 21:58:32 -0600

    Ayudantes para determinar el estado de un egreso

--- apps/common/templatetags/ayudantes.py
+++ apps/common/templatetags/ayudantes.py
@@ -1,10 +1,41 @@
 from django import template
-from django.contrib.auth.models import Group 
+from django.contrib.auth.models import Group
 from django.shortcuts import get_object_or_404
 
-register = template.Library() 
+register = template.Library()
+
 
 @register.filter(name='avatar')
 def avatar(user):
     avatar_perfil = user.perfil.foto.url
     return avatar_perfil
+
+
+@register.filter(name='estado_solicitud_egreso')
+def estado_solicitud_egreso(estado):
+    if estado == 'estado_pendiente':
+        return 'Pendiente'
+
+    elif estado == 'estado_aprovado':
+        return 'Aprovado'
+
+    elif estado == 'estado_denegado':
+        return 'Denegado'
+
+    else:
+        return 'Inválido'
+
+
+@register.filter(name='estado_lista')
+def estado_lista(estado):
+    if estado == 'estado_pendiente':
+        return None
+
+    elif estado == 'estado_aprovado':
+        return 'success'
+
+    elif estado == 'estado_denegado':
+        return 'warning'
+
+    else:
+        return 'danger'
