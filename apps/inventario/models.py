7dba8c39010566263021589fb5b28c45f4192c07
luishck <luisjcenteno@live.com>
2017-04-28 16:54:30 -0600

    Nuevos campos para articulos

--- apps/inventario/models.py
+++ apps/inventario/models.py
@@ -6,9 +6,14 @@ from django.utils import timezone
 class Articulos(models.Model):
     """Almacenar los detalles de los Articulos"""
     nombre = models.CharField(max_length=200)
+    codigo = models.CharField(max_length=20)
     descripcion = models.TextField(blank=True)
-    precio_compra = models.DecimalField(max_digits=6, decimal_places=2)
-    precio_venta = models.DecimalField(max_digits=6, decimal_places=2)
+    precio_compra = models.DecimalField(max_digits=7, decimal_places=2)
+    precio_venta = models.DecimalField(max_digits=7, decimal_places=2)
+    precio_venta2 = models.DecimalField(max_digits=7, decimal_places=2)
+    precio_venta3 = models.DecimalField(max_digits=7, decimal_places=2)
+    iva = models.DecimalField(max_digits=5, decimal_places=2)
+    cantidad_inicial = models.DecimalField(max_digits=7, decimal_places=2)
     fecha_creacion = models.DateTimeField(default=timezone.now)
     def __str__(self):
         return self.nombre
@@ -18,6 +23,7 @@ class Inventario(models.Model):
     articulo = models.ForeignKey('Articulos', on_delete=models.CASCADE,)
     existencias = models.IntegerField()
     minimo_existencias = models.IntegerField(default=1)
+    activo = models.BooleanField(default=True)
     fecha_creacion = models.DateTimeField(default=timezone.now)
     def __int__(self):
         return self.existencias
