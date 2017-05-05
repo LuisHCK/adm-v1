6644f13c487160acd8367891538e3ad5a257678d
luishck <luisjcenteno@live.com>
2017-05-04 21:50:29 -0600

    Renombrar columna

--- apps/facturas/models.py
+++ apps/facturas/models.py
@@ -12,15 +12,16 @@ class Factura(models.Model):
     usuario = models.ForeignKey('auth.User')
     cliente = models.CharField(max_length=100)
     total = models.DecimalField(max_digits=6, decimal_places=2, default=0, blank=True)
-    cobrada = models.BooleanField(default=False)
-    contado = models.BooleanField(default=True)
+    cerrada = models.BooleanField(default=False) # La factura un está siendo creada
+    contado = models.BooleanField(default=True) # La factura es al crédito o de contado
+    pagada = models.BooleanField(default=False) # la factura al crédito fue pagada por el cliente
     fecha_limite = models.DateTimeField(blank=True, null=True)
     fecha_factura = models.DateTimeField(default=timezone.now)
     fecha_cobro = models.DateTimeField(blank=True, null=True)
 
     def cobrar(self):
         '''Si la factura es pagada se guarda'''
-        self.cobrada = True
+        self.cerrada = True
         self.fecha_cobro = timezone.now()
         self.save()
 
@@ -39,25 +40,23 @@ class Factura(models.Model):
 
     def estado(self):
         '''Devuelve el estado en que se encuentra una factura'''
+        estado = object
         if self.contado:
             estado = {'class': 'success', 'mensaje': 'Factura de Contado'}
-            return estado
 
-        elif self.contado is False and self.fecha_limite < timezone.now():
-            estado = {'class': 'warning', 'mensaje': 'Factura de pendiente de cobro'}
-            return estado
-
-        elif self.contado is False and self.fecha_limite > timezone.now():
-            estado = {'class': 'danger', 'mensaje': 'Se venció la fecha limite de pago'}
-            return estado
-
-        elif self.contado is False and self.fecha_limite > timezone.now() and self.cobrada is True:
+        elif self.pagada is True:
             estado = {'class': 'success', 'mensaje': 'Factura pagada'}
-            return estado
+
+        elif self.contado is False and timezone.now() < self.fecha_limite:
+            estado = {'class': 'warning', 'mensaje': 'Factura de pendiente de cobro'}
+
+        elif self.contado is False and timezone.now() >= self.fecha_limite:
+            estado = {'class': 'danger', 'mensaje': 'Se venció la fecha limite de pago'}
 
         else:
             estado = {'class': 'warning', 'mensaje': 'La factura no es válida'}
-            return estado
+
+        return estado
 
 class FacturaItems(models.Model):
     '''Almacena individualmente los items de una Factura'''
