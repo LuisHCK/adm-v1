59849f3f9a73f3de9c77a4b284b2530949b6acb3
LuisHCK <luisjcenteno@live.com>
2017-05-17 17:46:50 -0600

    Documentar funciones

--- apps/common/validaciones.py
+++ apps/common/validaciones.py
@@ -1,10 +1,14 @@
+
 def es_administrador(user):
+    '''Verificar que el usuario es Administrador'''
     if user.groups.filter(name__in=['Administrador']) or user.is_staff:
         return True
     else:
         return False
 
+
 def es_cajero(user):
+    '''Verificar que el usuario es Cajero'''
     if user.groups.filter(name__in=['Cajero']):
         return True
     else:
