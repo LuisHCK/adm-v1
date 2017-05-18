8b74c04d5eb548b6920896155556cede200fe3d3
LuisHCK <luisjcenteno@live.com>
2017-05-18 12:34:13 -0600

    Los datos se envian correctamente a la API :'D

--- apps/cloud/views.py
+++ apps/cloud/views.py
@@ -1,16 +1,23 @@
 import json
-import urllib
+import requests
 
 from apps.ajustes.models import Ajuste
 
-def send_to_api(dictionary, user_id, model):
-    ajuste = Ajuste.objects.only("api_url", "api_key").get(pk=user_id)
-    api_url = ajuste.api_url+model
+
+def send_to_api(dictionary, model):
+    # Obtener datos de la API
+    ajuste = Ajuste.objects.only("api_url", "api_key").get(pk=1)
+
+    # Obtener url y token
+    api_url = ajuste.api_url + '/' + model
     api_token = ajuste.api_key
 
+    # Diccionario a enviar
     params = json.dumps(dictionary).encode('utf8')
-    request = urllib.request.Request(api_url, data=params,
-                                     headers={'Authorization':'Token token='+api_token,
-                                              'Content-Type':'application/json'})
-    response = urllib.request.urlopen(request)
-    print(response.read().decode('utf8'))
+
+    # Headers para acceso a la Api
+    headers = {'Authorization': 'Token token=' + api_token,
+               'Content-Type': 'application/json'}
+
+    response = requests.post(api_url, data=params, headers=headers)
+    #print(str(response) + "- api_url: " + api_url + "- api_token: " + api_token)
