526013eed3ae28d3df7f0a3ee0a8ed22c429760d
luishck <luisjcenteno@live.com>
2017-06-14 22:48:09 -0600

    Handle cloud synch error

--- apps/cloud/views.py
+++ apps/cloud/views.py
@@ -8,23 +8,28 @@ from apps.cash.models import Cash
 
 
 def send_to_api(dictionary, model):
-    # Obtener datos de la API
-    ajuste = Settings.objects.only("api_url", "api_key").get(pk=1)
+    try:
+        # Obtener datos de la API
+        ajuste = Settings.objects.only("api_url", "api_key").get(pk=1)
 
-    # Obtener url y token
-    api_url = ajuste.api_url + '/' + model
-    api_token = ajuste.api_key
+        # Obtener url y token
+        api_url = ajuste.api_url + '/' + model
+        api_token = ajuste.api_key
 
-    # Diccionario a enviar
-    params = json.dumps(dictionary).encode('utf8')
+        # Diccionario a enviar
+        params = json.dumps(dictionary).encode('utf8')
 
-    # Headers para acceso a la Api
-    headers = {'Authorization': 'Token token=' + api_token,
-               'Content-Type': 'application/json'}
+        # Headers para acceso a la Api
+        headers = {'Authorization': 'Token token=' + api_token,
+                   'Content-Type': 'application/json'}
 
-    send_task = threading.Thread(target=sender, args=(api_url, api_token, params, headers))
-    send_task.setDaemon(False)
-    send_task.start()
+        send_task = threading.Thread(target=sender, args=(
+            api_url, api_token, params, headers))
+        send_task.setDaemon(False)
+        send_task.start()
+
+    except:
+        print("<------ Failed to send data to server ------>")
 
 
 def sender(api_url, api_token, params, headers):
