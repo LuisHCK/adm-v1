33d1badcdfb2b6f36f95eafd51596cae6f73e677
luishck <luisjcenteno@live.com>
2017-06-14 23:00:35 -0600

    Move ajax request to js file

---
+++ static/js/sales_ajax.js
@@ -0,0 +1,46 @@
+    $("#form_vender").submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            url: $(this).attr('action'),
+            data: {
+                product: $('#id_product').val(),
+                quantity: $('#id_quantity').val(),
+                discount: $('#id_discount').val(),
+                csrfmiddlewaretoken: $(this).attr('token')
+            },
+            dataType: "json",
+            type: "POST",
+            // handle a successful response
+            error: function (json) {
+                $('#id_articulo').val(''); // remove the value from the input
+                console.log(json); // log the returned json to the console
+                toastr["error"]("No se pudo realizar la sale")
+            },
+
+            success: function (json) {
+                $('#id_articulo').val('');
+                console.log(json);
+                toastr["success"](json.result)
+
+                // Quitar las la ultima fila de la tabla
+                $('#tabla_ventas').prepend(
+                    `<tr>
+                    <th scope="row">` + json.sale_id +
+                    `</th>
+                    <td>` + json.product +
+                    `</td>
+                    <td>` + json.quantity +
+                    `</td>
+                    <td>` + json.sale_price +
+                    `</td>
+                    <td>` + json.total +
+                    `</td>
+                    <td>` + json.created_at +
+                    `</td>
+                    <td>` + json.seller +
+                    `</td>
+                </tr>`
+                );
+            },
+        });
+    });
