0293d74bcfd9fae73b4b53f2342013f141d5e30d
luishck <luisjcenteno@live.com>
2017-06-14 22:37:49 -0600

    Fixed sales

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.Start, name='Start'),
-    url(r'^venta_rapida/$', views.SaleAjax, name='SaleAjax'),
     url(r'^servicio_rapido/$', views.ServiceAjax, name='ServiceAjax'),
 ]
--- apps/sales/urls.py
+++ apps/sales/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^nueva/$', views.MakeSale, name='MakeSale'),
     url(r'^$', views.Sales, name='sales'),
+    url(r'^venta_rapida/$', views.SaleAjax, name='SaleAjax'),
 ]
